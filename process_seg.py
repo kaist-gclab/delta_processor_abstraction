68fcb5fa23456809f708869f8a684f326013a853
riella <wooriel@gmail.com>
2025-12-23 04:50:35 +0900

    added inst. for running process_seg

--- process_seg.py
+++ process_seg.py
@@ -28,32 +28,32 @@ os.makedirs(save_path, exist_ok=True)
 meshes, names = ut.read_mesh(gt_path) # read all meshes / sorted
 point_seg, eseg_name, seseg_name = ut.read_seg_res(seg_res_path)
 
-for i in range(360,380): # len(meshes)
+for i in range(0, 20): # please process 20 meshes at one run
     mesh = meshes[i]
     mesh = mesh.apply_scale(10.0)
     name = names[i]
     points = ut.get_vertex(mesh)
     faces = ut.get_face(mesh)
     cur_seg = point_seg[i] # get related face segmentation
-    num_lst = [1, 2, 1, 0, 4, 7, 0, 4, 9, 5, 8, 6, 0, 8, 1, 1, 7, 3, 3, 3] # human
-    num_lst = [2, 0, 0, 11, 7, 2, 6, 12, 6, 10, 6, 6, 3, 3, 8, 4, 0, 11, 7, 1] # cup
-    num_lst = [2, 0, 0, 7, 3, 1, 7, 0, 5, 2, 1, 5, 0, 1, 0, 1, 2, 4, 1, 0] # glasses
-    num_lst = [9, 7, 1, 1, 6, 6, 6, 7, 1, 8, 5, 2, 6, 6, 6, 2, 4, 1, 4, 6] # airplane
-    num_lst = [6, 4, 1, 9, 3,  2, 3, 2, 3, 3,  3, 1, 1, 0, 6,  0, 6, 4, 8, 3] # ants
-    num_lst = [0, 5, 6, 0, 2,  0, 2, 2, 0, 0,  5, 4, 0, 1, 0,  4, 0, 0, 0, 0] # chair
-    num_lst = [0, 4, 6, 2, 2,  4, 0, 7, 0, 1,  5, 4, 2, 4, 2,  1, 7, 12, 8, 5] # octopus
-    num_lst = [5, 1, 11, 3, 2,  3, 5, 2, 4, 9,  1, 4, 4, 5, 9,  5, 12, 10, 4, 2] # table
-    num_lst = [3, 6, 4, 5, 7, 10, 2, 1, 11, 11, 9, 3, 2, 4, 7, 9, 10, 10, 6, 8] # teddy bear
-    num_lst = [2, 0, 3, 5, 4,  1, 0, 7, 4, 6,  5, 7, 6, 4, 2,  2, 0, 2, 0, 9] # hand
-    num_lst = [5, 4, 7, 3, 4, 3, 6, 1, 4, 8, 5, 2, 1, 5, 1, 6, 1, 9, 3, 7] # piler
-    num_lst = [1, 10, 8, 3, 4,  0, 3, 3, 6, 1,  6, 7, 5, 4, 0,  4, 6, 2, 0, 0] # fish
-    num_lst = [2, 2, 4, 4, 10, 3, 0, 5, 0, 1, 9, 3, 4, 9, 0, 1, 3, 4, 3, 1] # bird
-    num_lst = [2, 3, 1, 2, 7, 0, 2, 2, 0, 2, 3, 1, 1, 2, 0, 0, 2, 6, 5, 1] # armadillo
-    num_lst = [7,0,2,7,1, 4,0,7,4,0, 4,0,2,2,7, 4,4,0,6,6] # bust
-    num_lst = [0,2,2,2,1, 0,0,9,0,1, 0,2,2,0,1, 7,0,0,2,0] # mech
-    num_lst = [0,2,0,3,0, 0,0,6,3,5, 0,0,0,0,0, 1,0,6,4,2] # bearing
-    num_lst = [1,2,4,5,1, 10,0,10,5,9, 5,1,10,10,5, 7,3,7,6,3] # vase
-    num_lst = [0,7,8,9,1, 3,2,3,4,3, 3,6,9,1,2, 4,0,5,3,12] # fourleg
+    num_lst = [1,2,1,0,4, 7,0,4,9,5, 8,6,0,8,1, 1,7,3,3,3] # human
+    # num_lst = [2,0,0,11,7, 2,6,12,6,10, 6,6,3,3,8, 4,0,11,7,1] # cup
+    # num_lst = [2,0,0,7,3, 1,7,0,5,2, 1,5,0,1,0, 1,2,4,1,0] # glasses
+    # num_lst = [9,7,1,1,6, 6,6,7,1,8, 5,2,6,6,6, 2,4,1,4,6] # airplane
+    # num_lst = [6,4,1,9,3, 2,3,2,3,3, 3,1,1,0,6, 0,6,4,8,3] # ants
+    # num_lst = [0,5,6,0,2, 0,2,2,0,0, 5,4,0,1,0, 4,0,0,0,0] # chair
+    # num_lst = [0,4,6,2,2, 4,0,7,0,1, 5,4,2,4,2, 1,7,12,8,5] # octopus
+    # num_lst = [5,1,11,3,2, 3,5,2,4,9, 1,4,4,5,9, 5,12,10,4,2] # table
+    # num_lst = [3,6,4,5,7, 10,2,1,11,11, 9,3,2,4,7, 9,10,10,6,8] # teddy
+    # num_lst = [2,0,3,5,4,  1,0,7,4,6,  5,7,6,4,2,  2,0,2,0,9] # hand
+    # num_lst = [5,4,7,3,4, 3,6,1,4,8, 5,2,1,5,1, 6,1,9,3,7] # piler
+    # num_lst = [1,10,8,3,4,  0,3,3,6,1, 6,7,5,4,0,  4,6,2,0,0] # fish
+    # num_lst = [2,2,4,4,10, 3,0,5,0,1, 9,3,4,9,0, 1,3,4,3,1] # bird
+    # num_lst = [2,3,1,2,7, 0,2,2,0,2, 3,1,1,2,0, 0,2,6,5,1] # armadillo
+    # num_lst = [7,0,2,7,1, 4,0,7,4,0, 4,0,2,2,7, 4,4,0,6,6] # bust
+    # num_lst = [0,2,2,2,1, 0,0,9,0,1, 0,2,2,0,1, 7,0,0,2,0] # mech
+    # num_lst = [0,2,0,3,0, 0,0,6,3,5, 0,0,0,0,0, 1,0,6,4,2] # bearing
+    # num_lst = [1,2,4,5,1, 10,0,10,5,9, 5,1,10,10,5, 7,3,7,6,3] # vase
+    # num_lst = [0,7,8,9,1, 3,2,3,4,3, 3,6,9,1,2, 4,0,5,3,12] # fourleg
     elem = num_lst[i%20] # elem = idx of cur_seg
     seg = cur_seg[elem] # face segmentation
     assert faces.shape[0] == cur_seg[elem].shape[0], "len vertices and len labels not same"
